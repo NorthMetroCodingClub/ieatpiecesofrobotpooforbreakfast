83d1b3f37028f67aa93ab5d683e2be19e6e0afe4
dustmouse <paul.palubinski@gmail.com>
2014-03-05 20:33:28 -0700

    Added RobotContext class.  Should avoid deadlocks now and moving back onto spawn tiles.

--- kettlekin.py
+++ kettlekin.py
@@ -13,13 +13,13 @@ class Robot:
         # Can change this to whatevs...
         self._opp_suicide_hp = 40
         self._opp_suicide_enemy_count = 3
+        self._robot_contexts = []
+        # Not really using this right now.  Just for ref.
+        self._roles = {1: 'grunt', 2: 'ranger'}
         # We can add more probabilities if we end up with more roles.  And find a tidier way to assign them.
         self._ranger_probability = 0.4
         # Need a better name than this.
         self._ranger_neighborhood_offset = 3
-        # Rangers are badasses that will do their toilet business right in the open.
-        self._rangers = []
-        self._grunts = {}
         # Grunts organize themselves into units at specific locations.
         self._grunt_locations = []
 
@@ -44,7 +44,7 @@ class Robot:
     def act(self, game):
         # Make sure all the robots know what their role is.  Can probably optimize this to
         # only fire when we've reached a turn that spawns new robots.
-        self.__set_robot_roles(game)
+        self.__initialize_robots(game)
 
         adjacent_enemies = self.__get_adjacent_enemies(game, self.location)
 
@@ -58,7 +58,7 @@ class Robot:
             # Also, right now, we are killing ourselves every time we get in a fight to the death.
             # Is that wise?  What if we are the survivor?  Even with a small amount of HP, it is worth staying alive.
             if self.hp <= self._suicide_hp:
-                return [self._act_suicide, self.location]
+                return self.__act(game, self._act_suicide, self.location)
 
             # 2. Commit opportunistic suicide - if we're surrounded by a bunch of baddies.
             # Not sure how frequently this will occur because if it's 4 dudes grinding on you,
@@ -66,7 +66,7 @@ class Robot:
             # of health, we could be dead before we get a chance to pull the pin.
             if len(adjacent_enemies) >= self._opp_suicide_enemy_count and \
                 self.hp <= self._opp_suicide_hp:
-                return [self._act_suicide, self.location]
+                return self.__act(game, self._act_suicide, self.location)
             
             # 3. Retreat!!
             # If we're in a death slot, get the hell out of there.
@@ -76,23 +76,26 @@ class Robot:
 
                 if len(friendly_adjacent_locations) > 0:
                     # Move to first friendly location in array.
-                    return [self._act_move, friendly_adjacent_locations[0]]
+                    return self.__act(game, self._act_move, friendly_adjacent_locations[0])
                 else:
                     # Get in the fetal position.  Or should we attack and go out gloriously?
-                    return [self._act_guard, self.location]
+                    return self.__act(game, self._act_guard, self.location)
 
             # 4. Attack!!
             # Sort array on robot HP to attack weakest enemies first.
             # We want to dispatch dudes on their last leg.
             sorted_enemies = sorted(adjacent_enemies.iteritems(), key=lambda x: x[1].hp)
 
-            return [self._act_attack, sorted_enemies[0][0]]
+            return self.__act(game, self._act_attack, sorted_enemies[0][0])
 
         # 5. Move it!
-        if self.robot_id in self._grunts.iterkeys():
-            next_location = rg.toward(self.location, self._grunts[int(self.robot_id)])
+        grunt_contexts = filter(lambda context: context.id == self.robot_id and 
+            context.role_id == 1, self._robot_contexts)
 
-            return self.__act_cautious_move(game, next_location)
+        if len(grunt_contexts) > 0:
+            next_location = rg.toward(self.location, grunt_contexts[0].target_location)
+
+            return self.__act(game, self._act_move, next_location)
         # Could explicitly check for rangers here, but what if some robots are unaccounted for?
         # Shouldn't happen if i had any talent as a programmer...
         # Anyway, I'd rather they do something than just stand around with their metallic thumbs up their asses.
@@ -112,22 +115,28 @@ class Robot:
                 next_location = rg.toward(self.location, weakest_enemy[0])
 
                 if len(self.__get_adjacent_enemies(game, next_location)) <= 1:
-                    return self.__act_cautious_move(game, next_location)
+                    return self.__act(game, self._act_move, next_location)
 
             # If our next move is into a death slot or no enemies, let's just chill for a sec and guard.
-            return [self._act_guard, self.location]
+            # Don't actually need this here right now since we'll hit the default behavior which is the same.
+            return self.__act(game, self._act_guard, self.location)
 
         # Default behavior.  For now.
-        return [self._act_guard, self.location]
+        return self.__act(game, self._act_guard, self.location)
 
-    def __set_robot_roles(self, game):
+    def __initialize_robots(self, game):
         for robot in game['robots'].itervalues():
             # If if if if... suck it.
             if robot.player_id == self.player_id:
-                if self.robot_id not in self._rangers and \
-                    self.robot_id not in self._grunts:
+                robot_contexts = filter(lambda context: context.id == 
+                    self.robot_id, self._robot_contexts)
+
+                # Can clean this up to avoid repeating crap.
+                if len(robot_contexts) == 0:
                     if random.random() <= self._ranger_probability:
-                        self._rangers.append(self.robot_id)
+                        robot_context = RobotContext(self.robot_id, role_id=2)
+
+                        self._robot_contexts.append(robot_context)
                     else:
                         # Find closest unit and assign robot to it.
                         grunt_location_distances = {}
@@ -136,23 +145,99 @@ class Robot:
                             grunt_location_distances[grunt_location] = \
                                 rg.dist(grunt_location, self.location)
 
-                        self._grunts[int(self.robot_id)] = min(grunt_location_distances, 
+                        target_location = min(grunt_location_distances, 
+                            key=lambda key: grunt_location_distances[key])
+
+                        robot_context = RobotContext(
+                            self.robot_id, role_id=1, target_location=target_location)
+
+                        self._robot_contexts.append(robot_context)
+                elif robot_contexts[0].role_id is None:
+                    robot_context = robot_contexts[0]
+
+                    if random.random() <= self._ranger_probability:
+                        robot_context.role_id = 2
+                    else:
+                        robot_context.role_id = 1
+
+                        # Find closest unit and assign robot to it.
+                        grunt_location_distances = {}
+
+                        for grunt_location in self._grunt_locations:
+                            grunt_location_distances[grunt_location] = \
+                                rg.dist(grunt_location, self.location)
+
+                        robot_context.target_location = min(grunt_location_distances, 
                             key=lambda key: grunt_location_distances[key])
 
         # Todo: clean up deceased robots from roles.
 
-    #Todo: resolve idiots constantly trying to enter the same cell.
-    def __act_cautious_move(self, game, move_location):
-        # First check if there is already someone at the location we want to move into.
+    def __act(self, game, action, location):
+        if (action == self._act_move):
+            return self.__cautious_move(game, location)
+        else:
+            robot_contexts = filter(
+                lambda context: context.id == self.robot_id, self._robot_contexts)
+
+            if len(robot_contexts) == 0:
+                robot_context = RobotContext(self.robot_id)
+                robot_context.add_action_history(action, location)
+
+                self._robot_contexts.append(robot_context)
+            else:
+                robot_contexts[0].add_action_history(action, location)
+
+            return [action, location]
+
+    # Is guarding as a default when our attempt at moving has failed a good idea?
+    # What other types of behavior are there?
+    def __cautious_move(self, game, move_location):
+        # Todo: expand on this.
+        # First, let's not allow our guys to move back onto a spawn
+        # location once they've moved inwards.
+        if 'spawn' not in rg.loc_types(self.location) and \
+            'spawn' in rg.loc_types(move_location):
+            return [self._act_guard, self.location]
+
+        robot_contexts = filter(
+            lambda context: context.id == self.robot_id, self._robot_contexts)
+        robot_context = None
+
+        if len(robot_contexts) == 0:
+            robot_context = RobotContext(self.robot_id)
+
+            self._robot_contexts.append(robot_context)
+        else:
+            robot_context = robot_contexts[0]
+
+        # Then, check if there is already someone at the location we want to move into.
         for location, robot in game.robots.iteritems():
             if location == move_location:
+                robot_context.add_action_history(self._act_guard, self.location)
+
                 return [self._act_guard, self.location]
 
-        # Next check if the move location is surrounded by at least two enemies.
+        # Next, check if the move location is surrounded by at least two enemies.
         if len(self.__get_adjacent_enemies(game, move_location)) > 1:
+            robot_context.add_action_history(self._act_guard, self.location)
+
             return [self._act_guard, self.location]
-        else:
-            return [self._act_move, move_location]
+
+        # Next, if we tried to move into the same location last time and it didn't
+        # work out, could be that we collided.  Let's just chill for a turn and
+        # see if that fixes things...  This is to avoid dead-locks.
+        last_action = robot_context.get_last_action()
+
+        if last_action is not None:
+            if last_action[0] == self._act_move and last_action[1] == move_location:
+                robot_context.add_action_history(self._act_guard, self.location)
+
+                return [self._act_guard, self.location]
+
+        # Dammit, looks like we're moving to the requested location.
+        robot_context.add_action_history(self._act_move, move_location)
+
+        return [self._act_move, move_location]
 
     def __get_adjacent_enemies(self, game, location):
         adjacent_enemies = {}
@@ -217,3 +302,25 @@ class Robot:
         y_max = location_y + neighborhood_offset
 
         return ((x_min, y_min), (x_max, y_max))
+
+# Keep track of context info for our robots.  Such as the history of their actions.
+# How about adding HP history to this bitch? 
+class RobotContext(object):
+    def __init__(self, robot_id, role_id = None, target_location = None):
+        self.id = robot_id
+        self.role_id = role_id
+        self.target_location = target_location
+        self._action_history = []
+
+    def add_action_history(self, action, location):
+        self._action_history.append((action, location))
+
+    def get_last_action(self):
+        if len(self._action_history) > 0:
+            return self._action_history[-1]
+
+    def get_action_at(self, index):
+        int_index = int(index)
+
+        if len(self._action_history) > int_index:
+            return self._action_history[int_index]
